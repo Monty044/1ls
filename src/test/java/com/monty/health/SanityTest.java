73d02293d63092328bccd4ef7385638b1b7704e8
kille <montadarabidali6@gmail.com>
2025-09-15 20:22:29 +0200

    sds

---
+++ src/test/java/com/monty/health/SanityTest.java
@@ -0,0 +1,10 @@
+package com.monty.health;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SanityTest {
+    @Test void pipelineRunsTests() {
+        assertEquals(4, 2 + 2);
+    }
+}
